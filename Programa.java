a6ac6615c7021015e38d75a7e4b1432682bc3959
luisaflorezm <luisa.florezm@upb.edu.co>
2021-05-06 13:10:34 -0500

    Agregar codigo del interfaz

--- Programa.java
+++
@@ -1,9 +0,0 @@
-public class Programa{
-public static void main(String[] args) {
-	
-System.out.println("  ___ __ __ _  ___ "  );
-System.out.println("/ ___|  |  ( \\/ __)"  );
-System.out.println("\\___ \\)(/    ( (_ \\");
-System.out.println("(____(__)_)__)\\___/"  );
-		}
-	}
