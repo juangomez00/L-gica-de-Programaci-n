2cb743de5d7112d5f2f729f886082e6a213346f0
luisaflorezm <luisa.florezm@upb.edu.co>
2021-05-05 23:19:02 -0500

    Agregar programa

---
+++ Programa.java
@@ -0,0 +1,9 @@
+public class Programa{
+public static void main(String[] args) {
+	
+System.out.println("  ___ __ __ _  ___ "  );
+System.out.println("/ ___|  |  ( \\/ __)"  );
+System.out.println("\\___ \\)(/    ( (_ \\");
+System.out.println("(____(__)_)__)\\___/"  );
+		}
+	}
