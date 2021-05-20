beede0bb94931ac6e6555be244ebeff9aecd2fee
juangomez00 <juan.gomezgi@upb.edu.co>
2021-05-19 19:18:51 -0500

    Corrigiendo información

--- Programa.java
+++ Programa.java
@@ -23,8 +23,8 @@ public class Programa{
         System.out.println("            /:/  /   \\:\\  \\:\\/:/  /     |::/  /   \\:\\/:/  / ;;;;;;             ");
         System.out.println("           /:/  /     \\:\\__\\::/  /      /:/  /     \\::/  /  `;;;;'               ");
         System.out.println("           \\/__/       \\/__/\\/__/       \\/__/       \\/__/                       ");
-        System.out.println("                                 ¡Bienvenido a SingAlong!");
-        System.out.println("                             Una rocola de tan solo un crédito");
+        System.out.println("                                  Bienvenido a SingAlong");
+        System.out.println("                             Una rocola de tan solo un credito");
         
  }
 
@@ -41,8 +41,8 @@ public class Programa{
             {
                 //case "a tildada mayuscula": caracter = '\u00C1';  Este case no nos funciona en nuestro ordenador.
                           //break;
-                case "á": caracter = '\u00E1';
-                          break;
+                //case "á": caracter = '\u00E1';
+                //          break;
                 case "é": caracter = '\u00E9';
                           break;
                 case "í": caracter = '\u00ED';
@@ -93,26 +93,26 @@ public class Programa{
         Scanner datos = new Scanner(System.in); 
         System.out.println(" ___|)________________________________________________________ ");
         System.out.println("|___/____________________________|___________________________||   Lista de Canciones ");
-        System.out.println("|__/|_______/|____/|_____/|______|___________________________||    1. Rosas ");
-        System.out.println("|_/(|,\\____/_|___/_|____/_|______|___________________________||    2. Me gustas tu ");
-        System.out.println("|_\\_|_/___|__|__|__|___|__|___|__|___________________________||    3. Rayando el sol ");
-        System.out.println("|   |     | ()  | ()   | ()   |  |                           ||    4. A Dios le pido ");
-        System.out.println("| (_|   -()-  -()-   -()-   -()- | -()-  -()-  -()-   -()-   ||    5. La camisa negra ");
-        System.out.println("|________________________________|__|__()_|__()_|__()__|_____||    6. Vivo por ella ");
-        System.out.println("|__/___\\_________________________|__|__|__|__|__|__|___|_____||    7. Maldito duende ");
-        System.out.println("|__\\___|_________________________|___\\_|___\\_|___\\_|___|_____||    8. En algun lugar ");
-        System.out.println("|_____/__________________________|____\\|____\\|____\\|_________||    9. Como camaron ");
-        System.out.println("|____/___________________________|___________________________||   10. La Gasolina ");
-        System.out.println("           _______       __                                       11. Dame amor ");
-        System.out.println("         /   ------.   / ._`_                                     12. Laura ");
-        System.out.println("        |  /         ~--~    \\                                    13. Nada valgo sin tu amor ");
-        System.out.println("        | |             __    `.____________________ _^-----^     14. Vinotinto ");
-        System.out.println("        | |  I=|=======/--\\=========================| o o o |     15. Cuando sea grande ");
-        System.out.println("        \\ |  I=|=======\\__/=========================|_o_o_o_|     16. Cielo ");
-        System.out.println("         \\|                   /                       ~    ~      17. La raja de tu falda ");
-        System.out.println("           \\       .---.    .                                     18. Hijo de la luna ");
-        System.out.println("             -----'     ~~''                                      19. Musica Ligera ");
-        System.out.println("                                                                  20. Oye mi amor ");
+        System.out.println("|__/|_______/|____/|_____/|______|___________________________||    1. Rosas - Oreja de Van Gogh  ");
+        System.out.println("|_/(|,\\____/_|___/_|____/_|______|___________________________||    2. Me gustas tu - Manu Chao");
+        System.out.println("|_\\_|_/___|__|__|__|___|__|___|__|___________________________||    3. Rayando el sol - Mana");
+        System.out.println("|   |     | ()  | ()   | ()   |  |                           ||    4. A Dios le pido - Juanes");
+        System.out.println("| (_|   -()-  -()-   -()-   -()- | -()-  -()-  -()-   -()-   ||    5. La camisa negra - Juanes");
+        System.out.println("|________________________________|__|__()_|__()_|__()__|_____||    6. Vivo por ella - Andrea Bocelli");
+        System.out.println("|__/___\\_________________________|__|__|__|__|__|__|___|_____||    7. Maldito duende - Heroes del Silencio");
+        System.out.println("|__\\___|_________________________|___\\_|___\\_|___\\_|___|_____||    8. En algun lugar - Ducan Dhu");
+        System.out.println("|_____/__________________________|____\\|____\\|____\\|_________||    9. Como camaron - Estopa");
+        System.out.println("|____/___________________________|___________________________||   10. La Gasolina - Daddy Yankee");
+        System.out.println("           _______       __                                       11. Dame amor - Erick Rubin");
+        System.out.println("         /   ------.   / ._`_                                     12. Laura - NEK");
+        System.out.println("        |  /         ~--~    \\                                    13. Nada valgo sin tu amor - Juanes");
+        System.out.println("        | |             __    `.____________________ _^-----^     14. Vinotinto - Estopa");
+        System.out.println("        | |  I=|=======/--\\=========================| o o o |     15. Cuando sea grande - Cuarteto de nos");
+        System.out.println("        \\ |  I=|=======\\__/=========================|_o_o_o_|     16. Cielo - Benny Ibarra");
+        System.out.println("         \\|                   /                       ~    ~      17. La raja de tu falda - Estopa");
+        System.out.println("           \\       .---.    .                                     18. Hijo de la luna - Mecano");
+        System.out.println("             -----'     ~~''                                      19. Musica Ligera - Soda Estereo");
+        System.out.println("                                                                  20. Oye mi amor - Mana");
         System.out.println(" ");
         imprimir("                Ingrese el numero de la canción que desee escuchar: ");  
         int numero = datos.nextInt();
